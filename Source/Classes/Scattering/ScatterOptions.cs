a24ea097bf0b14dd9d9c715bbc405b647857e67a
Mr. Teterew <shootatfriends@gmail.com>
2018-11-03 19:06:10 +0700

    added settings localization fields

---
+++ Source/Classes/Scattering/ScatterOptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RealRuins {
+    class ScatterOptions {
+        float densityMultiplier;
+        int referenceRadiusAverage;
+        float deteriorationMultiplier;
+        float scavengingMultiplier;
+
+        int itemCostLimit;
+        bool canSpawnItems;
+        bool wallsDoorsOnly;
+
+        float decorationChance; //probability PER CELL
+        float trapChance; //probability PER CELL
+        float hostileChance; //probability PER CHUNK
+    }
+}
