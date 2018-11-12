c30a9a1a12974ea26c413a2c73bebe5fb9042caf
Mr. Teterew <shootatfriends@gmail.com>
2018-11-12 21:02:17 +0700

    finalized pristine ruins event

--- Source/Classes/Incidents/RuinedBaseComp.cs
+++ Source/Classes/Incidents/RuinedBaseComp.cs
@@ -2,6 +2,9 @@ using RimWorld.Planet;
 
 namespace RealRuins {
     public class RuinedBaseComp : WorldObjectComp {
+
+        public bool isTriggeredForRaidAlready = false;
+        
         public RuinedBaseComp() {
             
         }
