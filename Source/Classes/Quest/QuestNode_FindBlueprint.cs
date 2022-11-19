7a2f350447398cbe79b4ac4954cc5c6b405fc1da
Igor Chertenkov <woolstrand@gmail.com>
2022-11-19 22:02:16 +0600

    fixed quest generation process

--- Source/Classes/Quest/QuestNode_FindBlueprint.cs
+++ Source/Classes/Quest/QuestNode_FindBlueprint.cs
@@ -7,16 +7,36 @@ namespace RealRuins
 	public class QuestNode_FindBlueprint : QuestNode
 	{
 
-		public SlateRef<string> storeAs;
+        public SlateRef<string> storeAs;
+        public SlateRef<string> storeCostAs;
 
-		public SlateRef<int> minimumWealth;
+        public SlateRef<int> minimumWealth;
 		public SlateRef<int> minimumArea;
 		public SlateRef<int> minimumItemsCount;
 
 		private string FindBlueprint(Slate slate) {
 			string filename;
-			BlueprintFinder.FindRandomBlueprintWithParameters(out filename, minimumArea.GetValue(slate), 0.01f, (int)Math.Min(minimumWealth.GetValue(slate), RealRuins_ModSettings.ruinsCostCap), maxAttemptsCount: 20);
-			Debug.Log(Debug.QuestNode_Find, "Found suitable blueprint");
+			Blueprint bp = BlueprintFinder.FindRandomBlueprintWithParameters(out filename, minimumArea.GetValue(slate), 0.01f, (int)Math.Min(minimumWealth.GetValue(slate), RealRuins_ModSettings.ruinsCostCap), maxAttemptsCount: 50);
+			if (bp != null) {
+				int intCost = (int)bp.totalCost;
+				if (intCost != 0) {
+					string key = storeCostAs.GetValue(slate);
+					if (key != null) {
+						slate.Set("blueprintCachedCost", (int)(bp.totalCost));
+						var storedCost = slate.Get<int>("blueprintCachedCost");
+                        Debug.Log(Debug.QuestNode_Find, "value: {0}, slate: {1}", storedCost, slate);
+
+						//success
+                        Debug.Log(Debug.QuestNode_Find, "Found suitable blueprint {0} of total cost {1}", filename, bp.totalCost);
+                    } else {
+                        Debug.Log(Debug.QuestNode_Find, "Key for cost cache value is null");
+                    }
+                } else {
+                    Debug.Log(Debug.QuestNode_Find, "Resulting blueprint cost = 0");
+                }
+            } else {
+                Debug.Log(Debug.QuestNode_Find, "Suitable blueprint not found");
+            }
 			return filename;
 		}
 
@@ -27,7 +47,9 @@ namespace RealRuins
 		protected override void RunInt() {
 			Slate slate = QuestGen.slate;
 			var filename = FindBlueprint(slate);
-			slate.Set(storeAs.GetValue(slate), filename);
+			if (filename != null) {
+				slate.Set(storeAs.GetValue(slate), filename);
+			}
 		}
 	}
 }
