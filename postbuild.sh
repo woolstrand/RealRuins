a4532d41c82999a517b289da72cde66cae8362a6
Igor Chertenkov <woolstrand@gmail.com>
2024-10-02 20:40:16 +0200

    1.5 update (better late than never)

--- postbuild.sh
+++ postbuild.sh
@@ -30,3 +30,15 @@ cp -R ../1.4/Assemblies/RealRuins.dll /Users/woolstrand/Library/Application\ Sup
 cp -R ../1.4/Defs /Users/woolstrand/Library/Application\ Support/Steam/steamapps/common/RimWorld/RimWorldMac.app/Mods/RealRuins/1.4
 
 cp -R ../1.4/Patches /Users/woolstrand/Library/Application\ Support/Steam/steamapps/common/RimWorld/RimWorldMac.app/Mods/RealRuins/1.4
+
+# --- 1.5 ---
+
+mkdir -p /Users/woolstrand/Library/Application\ Support/Steam/steamapps/common/RimWorld/RimWorldMac.app/Mods/RealRuins/1.5/Assemblies
+
+rm -f /Users/woolstrand/Library/Application\ Support/Steam/steamapps/common/RimWorld/RimWorldMac.app/Mods/RealRuins/1.5/Assemblies/*.dll
+
+cp -R ../1.5/Assemblies/RealRuins.dll /Users/woolstrand/Library/Application\ Support/Steam/steamapps/common/RimWorld/RimWorldMac.app/Mods/RealRuins/1.5/Assemblies/RealRuins.dll
+
+cp -R ../1.5/Defs /Users/woolstrand/Library/Application\ Support/Steam/steamapps/common/RimWorld/RimWorldMac.app/Mods/RealRuins/1.5
+
+cp -R ../1.5/Patches /Users/woolstrand/Library/Application\ Support/Steam/steamapps/common/RimWorld/RimWorldMac.app/Mods/RealRuins/1.5
